da94ed8668e94dba37ede1f36feb205c2a69c6b3
ZekariasGithub <101381002+ZekariasGithub@users.noreply.github.com>
2024-08-18 04:09:18 +0100

    Update App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Register from "./components/Register";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div>
+      <h1>Matchmaking App</h1>
+      <Register />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
